bc8af41da96464b1de562af3eeb3c593eaddee58
K.Kimura <dotnsf@gmail.com>
2021-12-08 16:52:06 +0900

    settings.js updated.

--- settings.js
+++ settings.js
@@ -8,7 +8,7 @@ exports.redis_port = 6379;
 exports.redis_db = '0';
 
 //. Cloudant
-exports.db_username = 'c0cc5a3d-992f-49a3-bd9a-0fb62f028a6d-bluemix';
-exports.db_password = 'dc7ec200b76596d1a1d0f9008ee231e366a2ddef31b1173e5caba8b1d8b0a474';
-exports.db_url = 'https://c0cc5a3d-992f-49a3-bd9a-0fb62f028a6d-bluemix.cloudant.com';
+exports.db_username = '';
+exports.db_password = '';
+exports.db_url = '';
 exports.db_name = 'doodleshareex';
