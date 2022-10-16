c19d44132f754c0fc1ac6693870d9d9f6518ad3c
K.Kimura <dotnsf@gmail.com>
2022-10-16 18:29:57 +0900

    demo.sh for demonstration purpose added.

---
+++ yaml/demo.sh
@@ -0,0 +1,26 @@
+
+ docker run --name aida -d -e UUID=相田 -e ROOM=dotnsf -e IMAGES_FOLDER=sample_images/a -e INTERVAL_MS=3000 -e SERVER_URL=wss://doodleshareex.yellowmix.net dotnsf/doodleshareex-client
+ sleep 2
+
+ docker run --name tachibana -d -e UUID=橘 -e ROOM=dotnsf -e IMAGES_FOLDER=sample_images/pi -e INTERVAL_MS=3000 -e SERVER_URL=wss://doodleshareex.yellowmix.net dotnsf/doodleshareex-client
+ sleep 2
+
+ docker run --name yamamoto -d -e UUID=山本 -e ROOM=dotnsf -e IMAGES_FOLDER=sample_images/ka -e INTERVAL_MS=3000 -e SERVER_URL=wss://doodleshareex.yellowmix.net dotnsf/doodleshareex-client
+ sleep 2
+
+ docker run --name koike -d -e UUID=小池 -e ROOM=dotnsf -e IMAGES_FOLDER=sample_images/sa -e INTERVAL_MS=3000 -e SERVER_URL=wss://doodleshareex.yellowmix.net dotnsf/doodleshareex-client
+ sleep 2
+
+ docker run --name suzuki -d -e UUID=鈴木 -e ROOM=dotnsf -e IMAGES_FOLDER=sample_images/p -e INTERVAL_MS=3000 -e SERVER_URL=wss://doodleshareex.yellowmix.net dotnsf/doodleshareex-client
+ sleep 2
+
+ docker run --name hashimoto -d -e UUID=橋本 -e ROOM=dotnsf -e IMAGES_FOLDER=sample_images/tsu -e INTERVAL_MS=3000 -e SERVER_URL=wss://doodleshareex.yellowmix.net dotnsf/doodleshareex-client
+ sleep 2
+
+ docker run --name inoue -d -e UUID=井上 -e ROOM=dotnsf -e IMAGES_FOLDER=sample_images/he -e INTERVAL_MS=3000 -e SERVER_URL=wss://doodleshareex.yellowmix.net dotnsf/doodleshareex-client
+ sleep 2
+
+ docker run --name ishiura -d -e UUID=石浦 -e ROOM=dotnsf -e IMAGES_FOLDER=sample_images/oji -e INTERVAL_MS=3000 -e SERVER_URL=wss://doodleshareex.yellowmix.net dotnsf/doodleshareex-client
+ sleep 2
+
+
