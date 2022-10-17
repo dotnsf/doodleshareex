2d72b2bd9a8927a4820ce907a1ea7d6a72f2b80d
K.Kimura <dotnsf@gmail.com>
2022-10-17 17:05:29 +0900

    name of user altered in yaml/demo.sh

--- yaml/demo.sh
+++ yaml/demo.sh
@@ -1,26 +1,26 @@
 
- docker run --name aida -d -e UUID=相田 -e ROOM=dotnsf -e IMAGES_FOLDER=sample_images/a -e INTERVAL_MS=3000 -e SERVER_URL=wss://doodleshareex.yellowmix.net dotnsf/doodleshareex-client
+ docker run --name kuroshima -d -e UUID=黒島 -e ROOM=dotnsf -e IMAGES_FOLDER=sample_images/a -e INTERVAL_MS=3000 -e SERVER_URL=wss://doodleshareex.yellowmix.net dotnsf/doodleshareex-client
  sleep 2
 
- docker run --name tachibana -d -e UUID=橘 -e ROOM=dotnsf -e IMAGES_FOLDER=sample_images/pi -e INTERVAL_MS=3000 -e SERVER_URL=wss://doodleshareex.yellowmix.net dotnsf/doodleshareex-client
+ docker run --name nounen -d -e UUID=能年 -e ROOM=dotnsf -e IMAGES_FOLDER=sample_images/pi -e INTERVAL_MS=3000 -e SERVER_URL=wss://doodleshareex.yellowmix.net dotnsf/doodleshareex-client
  sleep 2
 
- docker run --name yamamoto -d -e UUID=山本 -e ROOM=dotnsf -e IMAGES_FOLDER=sample_images/ka -e INTERVAL_MS=3000 -e SERVER_URL=wss://doodleshareex.yellowmix.net dotnsf/doodleshareex-client
+ docker run --name fukuhara -d -e UUID=福原 -e ROOM=dotnsf -e IMAGES_FOLDER=sample_images/ka -e INTERVAL_MS=3000 -e SERVER_URL=wss://doodleshareex.yellowmix.net dotnsf/doodleshareex-client
  sleep 2
 
- docker run --name koike -d -e UUID=小池 -e ROOM=dotnsf -e IMAGES_FOLDER=sample_images/sa -e INTERVAL_MS=3000 -e SERVER_URL=wss://doodleshareex.yellowmix.net dotnsf/doodleshareex-client
+ docker run --name kawaei -d -e UUID=川栄 -e ROOM=dotnsf -e IMAGES_FOLDER=sample_images/sa -e INTERVAL_MS=3000 -e SERVER_URL=wss://doodleshareex.yellowmix.net dotnsf/doodleshareex-client
  sleep 2
 
- docker run --name suzuki -d -e UUID=鈴木 -e ROOM=dotnsf -e IMAGES_FOLDER=sample_images/p -e INTERVAL_MS=3000 -e SERVER_URL=wss://doodleshareex.yellowmix.net dotnsf/doodleshareex-client
+ docker run --name kamishiraishi -d -e UUID=上白石 -e ROOM=dotnsf -e IMAGES_FOLDER=sample_images/p -e INTERVAL_MS=3000 -e SERVER_URL=wss://doodleshareex.yellowmix.net dotnsf/doodleshareex-client
  sleep 2
 
- docker run --name hashimoto -d -e UUID=橋本 -e ROOM=dotnsf -e IMAGES_FOLDER=sample_images/tsu -e INTERVAL_MS=3000 -e SERVER_URL=wss://doodleshareex.yellowmix.net dotnsf/doodleshareex-client
+ docker run --name fukatsu -d -e UUID=深津 -e ROOM=dotnsf -e IMAGES_FOLDER=sample_images/tsu -e INTERVAL_MS=3000 -e SERVER_URL=wss://doodleshareex.yellowmix.net dotnsf/doodleshareex-client
  sleep 2
 
- docker run --name inoue -d -e UUID=井上 -e ROOM=dotnsf -e IMAGES_FOLDER=sample_images/he -e INTERVAL_MS=3000 -e SERVER_URL=wss://doodleshareex.yellowmix.net dotnsf/doodleshareex-client
+ docker run --name okada -d -e UUID=岡田 -e ROOM=dotnsf -e IMAGES_FOLDER=sample_images/he -e INTERVAL_MS=3000 -e SERVER_URL=wss://doodleshareex.yellowmix.net dotnsf/doodleshareex-client
  sleep 2
 
- docker run --name ishiura -d -e UUID=石浦 -e ROOM=dotnsf -e IMAGES_FOLDER=sample_images/oji -e INTERVAL_MS=3000 -e SERVER_URL=wss://doodleshareex.yellowmix.net dotnsf/doodleshareex-client
+ docker run --name saito -d -e UUID=斉藤 -e ROOM=dotnsf -e IMAGES_FOLDER=sample_images/oji -e INTERVAL_MS=3000 -e SERVER_URL=wss://doodleshareex.yellowmix.net dotnsf/doodleshareex-client
  sleep 2
 
 
